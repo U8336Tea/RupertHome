3f543cc3827ba4863bdd91baf5360cbd1f91fa51
U8336Tea <20009895+U8336Tea@users.noreply.github.com>
2021-01-19 20:22:25 -0800

    Random chance not to send the bot response

--- responses.rb
+++ responses.rb
@@ -94,6 +94,8 @@ def get_reply(message)
     if content.length < 5 || rand(0..127) == 0 # Message too short (Or randomly selected)
         return nil
     elsif content.include? ' bot' then
+        return RESPONSES.sample if rand(0..7) == 0
+
         return "Stop saying I'm a bot and get an actual argument for once in your goddamn life."
     elsif !content.match?(/[a-zA-Z]/) then # User sent a message like "..."
         return nil
